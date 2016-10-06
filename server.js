b7f6cda70262ee853ab05e242847457d07eada37
Thales Minussi <tminussi@gmail.com>
2016-10-05 21:33:21 -0300

    init socket e materialize

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-var PORT = process.env.port || 3000;
+var PORT = process.env.PORT || 3000;
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(PORT, function () {
+io.on('connection', function () {
+    console.log('User connected via socket.io');
+});
+
+http.listen(PORT, function () {
     console.log('Server started')
 });
