78f03323d83e45dbd82342c214c0b5ceb8ab2ef8
Thales Minussi <tminussi@gmail.com>
2016-10-07 21:29:03 -0300

    introducing live chat

--- public/js/app.js
+++ public/js/app.js
@@ -5,8 +5,7 @@ socket.on('connect', function (socket) {
 });
 
 socket.on('message', function (message) {
-    console.log('New message: ');
-    console.log(message.text);
+    $('.messages').append('<p>' + message.text + '</p>')
 });
 
 var $form = jQuery('#message-form');
