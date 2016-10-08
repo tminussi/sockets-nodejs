6e1894df7afc81cde3bc1bdd8403a0569adeab95
Thales Minussi <tminussi@gmail.com>
2016-10-08 09:24:10 -0300

    adding names and change settings to the chat

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,15 @@
 var socket = io();
-
+var name = getQueryVariable('name');
+var room = getQueryVariable('room');
 socket.on('connect', function (socket) {
     console.log('Connected to socket.io server');
 });
 
 socket.on('message', function (message, timeSent) {
-    $('.messages').append('<p>' + '<strong>' + timeSent + '</strong>' + ': ' + message.text + '</p>')
+    $('.messages').append('<p>' + name + ' - ' + '<strong>' + timeSent + '</strong>' + ': ' + message.text + '</p>')
 });
 
-var $form = jQuery('#message-form');
+var $form = $('#message-form');
 $form.on('submit', function (event) {
     event.preventDefault();
     var message = $form.find('input[name=message]');
@@ -18,3 +19,15 @@ $form.on('submit', function (event) {
 
     message.val('').focus();
 });
+
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var args = query.split('&');
+    for (var i = 0; i < args.length; i++) {
+        var pair = args[i].split('=');
+        if (decodeURIComponent(pair[0]) == variable) {
+            return decodeURIComponent(pair[1]);
+        }
+    }
+    return undefined;
+}
